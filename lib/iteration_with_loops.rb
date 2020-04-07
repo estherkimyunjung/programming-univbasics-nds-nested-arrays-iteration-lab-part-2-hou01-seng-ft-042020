1a7d4b8113a3ae0c30b262c7db07309c84416943
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-07 06:03:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,19 +6,23 @@ def find_min_in_nested_arrays(src)
   
   new_arry = []
   row_index = 0 
+  
   while row_index < src.length do
     column_index = 0 
     min_num = 1000000000
+    
     while column_index < src[row_index].length do
       
       if min_num > src[row_index][column_index]
-      min_num = src[row_index][column_index]
-    end
+        min_num = src[row_index][column_index]
+      end
+      
       column_index += 1 
     end
+    
     new_arry << min_num
     row_index += 1 
-    
   end
+  
   new_arry
 end
